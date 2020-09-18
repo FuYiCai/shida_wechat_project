f2276a4211643dcd9748df0e4693631bdd74b6c1
新-岸 <1400330380@qq.com>
2020-09-18 15:17:03 +0800

    addLogo

--- siteinfo.js
+++ siteinfo.js
@@ -1,5 +1,5 @@
 module.exports = {
   name: "萤火小程序商城",
-  siteroot: "http://192.168.2.156:8083/", // 必填: api地址
+  siteroot: "http://192.168.2.156:8082/", // 必填: api地址
   uniacid: "10001", // 此处为商城ID, 可在超管后台-商城列表中查看
 };
